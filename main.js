90c44a0124aecfd1047c6cb3203f45eef2c77c19
Jed <blady209@gmail.com>
2022-06-27 19:24:24 +0200

    polidrome

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 //Reverse string
-
 const reverseString = (text) => {
   const text_array = text.split("");
   text_array.reverse();
@@ -16,3 +15,23 @@ const reverseString2 = (text) => {
   return reversed;
 };
 // console.log(reverseString2("dom"));
+
+//
+// Polidrome
+const isPolidorme = (word) => {
+  return word === word.split("").reverse().join("") ? true : false;
+};
+console.log(isPolidorme("kajak"));
+
+const isPolidorme2 = (word) => {
+  for (let i = 0; i < Math.floor(word.length / 2); i++) {
+    if (word[i] !== word[word.length - i - 1]) {
+      console.log(word[i], word[word.length - 1]);
+      return false;
+    }
+  }
+  return true;
+};
+console.log(isPolidorme2("kajakk"));
+
+//git push -u origin main
