bfac91be77ba69e4605a624f0c25cd741c7a443e
Jed <blady209@gmail.com>
2022-07-03 21:34:31 +0200

    fizzbuzz

--- main.js
+++ main.js
@@ -81,4 +81,21 @@ function disemvowel(str) {
   }
 }
 
+//
+// Fizz buzz
+const fizzBuz = (number) => {
+  for (let i = 1; i < number + 1; i++) {
+    if (i % 15 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else {
+      console.log(i);
+    }
+  }
+};
+// fizzBuz(16);
+
 //git push -u origin main
